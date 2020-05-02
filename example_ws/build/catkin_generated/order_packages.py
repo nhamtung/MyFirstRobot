85d752372677791ecaad069b269b32ad9a119af5
nhamtung <nhamtung125@gmail.com>
2020-05-02 02:09:30 +0000

    TestMPU6050

--- example_ws/build/catkin_generated/order_packages.py
+++ example_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/ubuntu/TungNV/MyFirstRobot/example_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/ubuntu/TungNV/MyFirstRobot/example_ws/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ubuntu/TungNV/MyFirstRobot/example_ws/devel;/home/ubuntu/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/ubuntu/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/ubuntu/catkin_ws/devel;/opt/ros/kinetic" != "" else []
