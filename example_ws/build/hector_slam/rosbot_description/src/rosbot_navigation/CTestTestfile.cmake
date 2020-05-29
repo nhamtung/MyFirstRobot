b00467e99b7b08573a31cff2a6570eb6b937ad98
nhamtung <nhamtung125@gmail.com>
2020-05-30 01:05:48 +0700

    save

---
+++ example_ws/build/hector_slam/rosbot_description/src/rosbot_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nhamtung/TungNV/MyFirstRobot/example_ws/src/hector_slam/rosbot_description/src/rosbot_navigation
+# Build directory: /home/nhamtung/TungNV/MyFirstRobot/example_ws/build/hector_slam/rosbot_description/src/rosbot_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ example_ws/src/hector_slam/rosbot_description/README.txt
@@ -0,0 +1,1 @@
+link: https://github.com/husarion/rosbot_description
